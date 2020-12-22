0ebca1923e2064f4572f2367ff9392911a9f267f
DarkBrave <DarkBrave@users.noreply.github.com>
2020-12-22 13:27:32 -0600

    starship page is broken

---
+++ starship/latest-video.js
@@ -0,0 +1,7 @@
+var channelID = "UCFwMITSkc1Fms6PoJoh1OUQ";
+var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+$.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
+   var link = data.items[0].link;
+   var id = link.substr(link.indexOf("=")+1);
+$("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
+});
